2cd3c1616dbd49714dfa4b96bd85fafb0a87d285
setiawanrizky <wicaksonosetiawan43@gmail.com>
2026-07-26 11:09:17 +0700

    Fix all hardcoded localhost API endpoints

--- myproject/yudisium-fe/src/api.js
+++ myproject/yudisium-fe/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || process.env.REACT_APP_API_URL || 'https://yudisium.pythonanywhere.com/api/'
+  baseURL: 'https://yudisium.pythonanywhere.com/api/'
 });
 
 // INTERCEPTOR: Otomatis tempelkan token ke Header setiap kali panggil API
